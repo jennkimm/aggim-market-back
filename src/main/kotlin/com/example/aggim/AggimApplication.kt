3ec8c1ade2d3443abab6631b0f5a3bf5e487ce76
smartshk <seohkim@student.42seoul.kr>
2021-01-21 22:52:15 +0900

    initial commit

---
+++ src/main/kotlin/com/example/aggim/AggimApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aggim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+open class AggimApplication
+
+fun main() {
+    runApplication<AggimApplication>()
+}
